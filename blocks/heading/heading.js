e3b8906dabe07e5342a3242ca0b39f9e75487449
Ajaykumar Prajapati <ajaykumar.prajapati@BVIAHMLAP-243.brainvire.com>
2026-06-17 12:15:43 +0530

    new title-desc block

--- blocks/heading/heading.js
+++
@@ -1,8 +0,0 @@
-export default function decorate(block) {
-  const text = block.textContent.trim();
-  const headingDiv = document.createElement('div');
-  headingDiv.classList.add('custom-heading');
-  headingDiv.textContent = text;
-  block.innerHTML = '';
-  block.appendChild(headingDiv);
-}
